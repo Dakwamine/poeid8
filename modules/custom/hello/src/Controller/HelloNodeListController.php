2b9e290e725f36f9e1229cbf40a23180769a6d56
User 3 <user3@localhost>
2017-02-03 15:28:33 +0100

    Dependency injection style!

--- modules/custom/hello/src/Controller/HelloNodeListController.php
+++ modules/custom/hello/src/Controller/HelloNodeListController.php
@@ -3,13 +3,28 @@
 namespace Drupal\hello\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Entity\Query\QueryFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class HelloNodeListController extends ControllerBase {
 
+  /**
+   * @var Drupal\Core\Entity\Query\QueryFactory
+   */
+  protected $queryFactory;
+
+  /**
+   * @var Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  public function __construct(QueryFactory $query_factory, EntityTypeManager $entity_type_manager) {
+    $this->queryFactory = $query_factory;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
   public function content($nodeType) {
-    // Check node type validity
-    
-    
     switch ($nodeType) {
       case '':
         return array(
@@ -19,25 +34,33 @@ class HelloNodeListController extends ControllerBase {
               )
           )
         );
-        
+
       default:
         // Load page nodes
-        $ids = \Drupal::entityQuery('node')->condition('type', $nodeType)->pager(15)->execute();
-        $entities = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple($ids);
-        
+        $ids = $this->queryFactory->get('node')->condition('type', $nodeType)->pager(15)->execute();
+        $entities = $this->entityTypeManager->getStorage('node')->loadMultiple($ids);
+
         $content = [];
-        
-        foreach($entities as $e) {
+
+        foreach ($entities as $e) {
+          // The easy way
           $content[] = $e->toLink();
+          
+          // The hard way
           //$content[] = Link::createFromRoute($node->label(), 'entity.node.canonical', array('node' => $node->id()));
         }
-        //kint($content);die();
-        
+
         $list = array('#theme' => 'item_list', '#items' => $content);
         $pager = array('#type' => 'pager');
-        
+
         return array($list, $pager);
     }
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+        $container->get('entity.query'), $container->get('entity_type.manager')
+    );
   }
 
 }
